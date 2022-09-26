8e3f94b164b97b443d22cf190a2bfe53de7f80df
KenGezuz <kennykhosa22@outlook.com>
2022-09-26 19:17:10 +0200

    status code error handler

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@
     const express = require('express');
         //file to hide private content 
     const dotenv = require('dotenv').config();
+         //import status code error middleware
+     const {errorHandler} = require('./middleware/errorHandler');
         //Access port name from .env 
         //5000 if first value not found
     const port = process.env.PORT || 5000
+
         
 
          //Initialize express
@@ -17,6 +20,9 @@
 
          //points to routes/milestoneRoutes for create,write,update and delete
     app.use('/api/milestone',require('./routes/milestoneRoutes'));
+
+         //use own error handlers
+     app.use(errorHandler);
     
          //run server and output port name
     app.listen(port, () => console.log(`Server is running on ${port}`));
