ed6282327daa93f3b8837b219fc67375a0bf418b
KenGezuz <kennykhosa22@outlook.com>
2022-09-26 17:10:23 +0200

    configure server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("lets built mile-stone");
+         //Backend framework 
+    const express = require('express');
+        //file to hide private content 
+    const dotenv = require('dotenv').config();
+        //Access port 
+    const port = 5000;
+        
+
+         //Initialize express
+    const app = express();
+         //run server and output port name
+    app.listen(port, () => console.log(`Server is running on ${port}`));
