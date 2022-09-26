102f9b19707b4ac65c18790fcb6b99bc0a91f914
KenGezuz <kennykhosa22@outlook.com>
2022-09-26 17:37:39 +0200

    output http request from server

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,16 @@
         //file to hide private content 
     const dotenv = require('dotenv').config();
         //Access port name from .env 
-    const port = process.env.PORT;
+        //5000 if first value not found
+    const port = process.env.PORT || 5000
         
 
          //Initialize express
     const app = express();
+
+         //request localhost:5000/api/milestone on postman and respond with message array 
+    app.get('/api/milestone', (req, res) => {
+        res.status(200).json({message : 'Message recieved'})  
+      });
          //run server and output port name
     app.listen(port, () => console.log(`Server is running on ${port}`));
